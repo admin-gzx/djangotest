73681640cb38a63d9ed81f375a8f711d18872778
admin-gzx <3105216868@qq.com>
2025-08-07 14:29:18 +0800

    修复了模板问题以及slug为空的问题

--- user_auth_system/settings.py
+++ user_auth_system/settings.py
@@ -45,8 +45,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'accounts' / 'templates',
-                 BASE_DIR / 'templates'  ],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
